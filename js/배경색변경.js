4688be16cd3469c915148b2cbfa6291299842cd9
hojunahn <hojunahn@naver.com>
2024-04-25 12:15:15 +0900

    js버튼배경색변경

---
+++ js/배경색변경.js
@@ -0,0 +1,19 @@
+    ``
+// let colorBtn = document.getElementById("colorButton");
+// const colors = ["red","orenge","yellow","green","blue","navy","purple"];
+// let colorIdx = 0;
+
+
+// colorBtn.addEventListener("click", () => {
+//     document.body.style.backgroundColor = colors[colorIdx];
+//     colorIdx = (colorIdx +1) % colors.length;
+// });
+
+// jQurey
+    var colors = ["red", "orange", "yellow", "green", "blue", "purple"];
+    var colorIndex = 0;
+  
+    $("#colorButton").click(function() {
+      $("body").css("background-color", colors[colorIndex]);
+      colorIndex = (colorIndex + 1) % colors.length;
+    });
